daec645b12100ef33adce5b7ccf6e3188a07049c
fdeboo <frances.deboo@gmail.com>
2020-05-03 20:00:19 +0100

    Basic app in place

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ from flask_login import LoginManager, login_user, login_required, current_user, 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello world"
+
+if __name__ == "__main__":
+    app.run(debug="True")
