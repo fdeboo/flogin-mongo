7f6752ca954ad2e86a9086baf9e6b57db9ee8abd
Frances <frances.deboo@gmail.com>
2020-05-06 15:45:25 +0100

    Login route created with login form

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash
 from flask_login import current_user
 from app.modules import User
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, LoginForm
 from app import app, mongo 
 
 
@@ -22,21 +22,18 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
+    users = mongo.db.users
     if form.validate_on_submit():
-        users = mongo.db.users
-        existing_user = users.find_one({'email': request.form['email']})
-        if existing_user is None:
-            hashed_password = generate_password_hash(form.password.data)
-            user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-            users.insert({'name': form.username.data, 'email': form.email.data, 'password': hashpass})
-            session['username'] = form.username.data
-            flash('You are now registered and can log in', 'success')
-            return redirect(url_for('login'))
-        flash('That email already exists!', 'danger')
+        hashed_password = generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        users.insert({'name': form.username.data, 'email': form.email.data, 'password': hashed_password})
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
