126bd3347477699c907aad1cdc847ee7c685d8bc
Frances <frances.deboo@gmail.com>
2020-05-06 11:41:17 +0100

    User class created and Mongo database initialised

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config.from_object(Config)
-
+mongo = PyMongo(app)
 login_manager = LoginManager(app)
 login_manager.login.view = 'login'
 login_manager.login_message_category = 'info'
